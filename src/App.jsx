5e43c8d66b05127e7f5c4c95de64888fea32f338
mhmmdfadhell <fadhel.muhammad02@gmail.com>
2023-06-12 15:12:08 +0700

    component-props

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ComponentProps from "./components/ComponentProps";
+function App(){
+  return(
+    //<> simbol untuk fragment
+    <>
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <ComponentProps textColor="text-lime-100" text="Hallo Gais"/>
+    </div>
+  
+    </>
+  ) 
+}
+export default App 
